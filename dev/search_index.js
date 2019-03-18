5aa746132a9167d4144d01e24ab0719b0f356567
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-03-18 23:14:20 +0000

    build based on 0c69e89

---
+++ dev/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#CLIMA-1",
+    "page": "Home",
+    "title": "CLIMA",
+    "category": "section",
+    "text": "Climate Machine"
+},
+
+]}
