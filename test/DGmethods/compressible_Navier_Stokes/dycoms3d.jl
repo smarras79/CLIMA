10a4a0695aadfda6d3955015c14cd85640e3ddc4
SIMONE MARRAS <simone@SIMONEs-MacBook-Pro.local>
2019-07-02 18:50:31 -0700

    test: remove CFL calculation from dycoms for speed test

--- test/DGmethods/compressible_Navier_Stokes/dycoms3d.jl
+++ test/DGmethods/compressible_Navier_Stokes/dycoms3d.jl
@@ -754,7 +754,7 @@ function run(mpicomm, dim, Ne, N, timeend, DFloat, dt)
 
 # Initialise the integration computation. Kernels calculate this at every timestep?? 
 @timeit to "initial integral" integral_computation(spacedisc, Q, 0) 
-@timeit to "solve" solve!(Q, lsrk; timeend=timeend, callbacks=(cbinfo, cbvtk, cbdt))
+@timeit to "solve" solve!(Q, lsrk; timeend=timeend, callbacks=(cbinfo, cbvtk))
 
 
 @info @sprintf """Finished...
