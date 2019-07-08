3dc7b6f0a95b2a216f2341cd15cdd9870a828052
SIMONE MARRAS <simone@SIMONEs-MacBook-Pro.local>
2019-07-07 21:51:58 -0700

    minor

--- test/DGmethods/compressible_Navier_Stokes/squall-line-WORKING-last-week.jl
+++ test/DGmethods/compressible_Navier_Stokes/squall-line-WORKING-last-week.jl
@@ -1004,8 +1004,7 @@ function run(mpicomm, dim, Ne, N, timeend, DFloat, dt)
 
 # Initialise the integration computation. Kernels calculate this at every timestep??
 #@timeit to "initial integral" integral_computation(spacedisc, Q, 0)
-#@timeit to "solve" solve!(Q, lsrk; timeend=timeend, callbacks=(cbinfo, cbvtk))
-@timeit to "solve" solve!(Q, lsrk; timeend=timeend, callbacks=cbinfo)
+@timeit to "solve" solve!(Q, lsrk; timeend=timeend, callbacks=(cbinfo, cbvtk))
 
 
 @info @sprintf """Finished...
