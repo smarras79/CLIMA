701b3aefe8e29fcf304ec906acd1f795e79558aa
SIMONE MARRAS <simone@SIMONEs-MacBook-Pro.local>
2019-07-08 14:03:20 -0700

    removed undefined index

--- test/DGmethods/compressible_Navier_Stokes/dycoms3d.jl
+++ test/DGmethods/compressible_Navier_Stokes/dycoms3d.jl
@@ -756,7 +756,7 @@ function run(mpicomm, dim, Ne, N, timeend, DFloat, dt)
                     R[_u] = u
                     R[_v] = v
                     R[_w] = w
-                    R[_q_liq] = aux[_a_q_iq]
+                    R[_q_liq] = aux[_a_q_liq]
                     #R[_T] = aux[_a_q_T]
                 end
             end
