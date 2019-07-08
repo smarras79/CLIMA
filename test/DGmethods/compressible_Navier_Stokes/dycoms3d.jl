26f94af144e030b96456148c59910e7f9931d925
SIMONE MARRAS <simone@SIMONEs-MacBook-Pro.local>
2019-07-08 13:10:54 -0700

    vtk every 1 step for debugging

--- test/DGmethods/compressible_Navier_Stokes/dycoms3d.jl
+++ test/DGmethods/compressible_Navier_Stokes/dycoms3d.jl
@@ -743,7 +743,7 @@ function run(mpicomm, dim, Ne, N, timeend, DFloat, dt)
         postprocessarray = MPIStateArray(spacedisc; nstate=npoststates)
 
         step = [0]
-        cbvtk = GenericCallbacks.EveryXSimulationSteps(1000) do (init=false)
+        cbvtk = GenericCallbacks.EveryXSimulationSteps(1) do (init=false)
             DGBalanceLawDiscretizations.dof_iteration!(postprocessarray, spacedisc, Q) do R, Q, QV, aux
                 @inbounds let
                     F_rad_out = radiation(aux)
