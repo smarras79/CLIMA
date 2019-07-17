d233362b874a6da506ff4d504c99ced559754049
SIMONE MARRAS <simone@SIMONEs-MacBook-Pro.local>
2019-07-17 11:33:06 -0700

    dt = 0.001

--- test/DGmethods/compressible_Navier_Stokes/dycoms2d.jl
+++ test/DGmethods/compressible_Navier_Stokes/dycoms2d.jl
@@ -831,7 +831,7 @@ let
   # User defined simulation end time
   # User defined polynomial order 
   numelem = (Nex,Ney,Nez)
-  dt = 0.0025
+  dt = 0.001
   #timeend = 4*dt
   timeend = 14400
   polynomialorder = Npoly
