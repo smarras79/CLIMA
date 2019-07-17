ed3b44d5fb05697dc910fafc75de2fa86a99aa3d
SIMONE MARRAS <simone@SIMONEs-MacBook-Pro.local>
2019-07-17 11:26:51 -0700

    vtk every 5000 steps

--- test/DGmethods/compressible_Navier_Stokes/dycoms2d.jl
+++ test/DGmethods/compressible_Navier_Stokes/dycoms2d.jl
@@ -764,7 +764,7 @@ function run(mpicomm, dim, Ne, N, timeend, DFloat, dt)
       end
       =#
       step = [0]
-      cbvtk = GenericCallbacks.EveryXSimulationSteps(1000) do (init=false)
+      cbvtk = GenericCallbacks.EveryXSimulationSteps(5000) do (init=false)
           DGBalanceLawDiscretizations.dof_iteration!(postprocessarray, spacedisc, Q) do R, Q, QV, aux
               @inbounds let
                   u, v, w = preflux(Q, QV, aux)
