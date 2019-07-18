94e85d0432146915746f889f5636867df004d0f3
SIMONE MARRAS <simone@SIMONEs-MacBook-Pro.local>
2019-07-18 13:22:08 -0700

    VFP[_Ty]   .= VFM[_Ty]

--- test/DGmethods/compressible_Navier_Stokes/dc_visc.jl
+++ test/DGmethods/compressible_Navier_Stokes/dc_visc.jl
@@ -360,19 +360,20 @@ end
         QP[_U] = UM - 2 * nM[1] * UnM
         QP[_V] = VM - 2 * nM[2] * UnM
         QP[_W] = WM - 2 * nM[3] * UnM
-        VFP   .= VFM
+        VFP[_Ty]   .= VFM[_Ty]
+        VFP[_Tz]   .= VFM[_Tz]
         #VFP .= 0
         #
         # B.C. that should prevent the thermal boundary layer from forming
         #
-        #QP[_ρ] = ρM  #this is:  dρ/dn = 0  i.e. ρ+ = ρ-
-        #QP[_E] = EM  #this is:  dE/dn = 0  i.e. E+ = E-                
+        QP[_ρ] = ρM  #this is:  dρ/dn = 0  i.e. ρ+ = ρ-
+        QP[_E] = EM  #this is:  dE/dn = 0  i.e. E+ = E-                
         #VFP   .= VFM #This means that stress tau at the boundary is zero (notice
                     #  that we are solving a viscous problem (nu=75) with a slip boundary; clearly this is physically incosistent but it will do for the sake of this benchmark (Straka 1993).
-        Pr = 0.7
-        ν = 75
-        VFP[_Ty] = -grav*Pr/(ν*cv_d*cp_d)
-        VFP[_Tz] = -grav*Pr/(ν*cv_d*cp_d)
+        #Pr = 0.7
+        #ν = 75
+        #VFP[_Ty] = -grav*Pr/(ν*cv_d*cp_d)
+        #VFP[_Tz] = -grav*Pr/(ν*cv_d*cp_d)
         
         nothing
     end
