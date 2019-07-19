39a7894ede75b7c25f130ef744ff88c769ff6ce6
SIMONE MARRAS <simone@SIMONEs-MacBook-Pro.local>
2019-07-19 14:39:39 -0700

    Added sponge 3: from Pycles

--- test/DGmethods/compressible_Navier_Stokes/test-dycoms-visc-2d.jl
+++ test/DGmethods/compressible_Navier_Stokes/test-dycoms-visc-2d.jl
@@ -384,7 +384,7 @@ end
 
    
       #Vertical sponge:
-      sponge_type = 2
+      sponge_type = 3
       if sponge_type == 1
           
           top_sponge  = DFloat(0.85) * domain_top          
