2dd526cb23dbba02d16d082f38dda31dd9222e09
SIMONE MARRAS <simone@SIMONEs-MacBook-Pro.local>
2019-07-31 17:09:25 -0700

    3d: * As per stevens:  S[_E] += (115 + 15)/(rho * Deltaz) * S[_QT] += 115/(rho * LH_v0 * Deltaz)

--- test/DGmethods/compressible_Navier_Stokes/test-dycoms-visc-3d.jl
+++ test/DGmethods/compressible_Navier_Stokes/test-dycoms-visc-3d.jl
@@ -632,7 +632,8 @@ end
             
             S[_U]  += dτ13dn 
             S[_V]  += dτ23dn
-            S[_E]  += (15 + 115)/(ρ * h_first_layer)
+            
+            S[_E]  += (15 + 115)/(ρ * h_first_layer)            
             #S[_E]  += SHF + LHF
             S[_QT] += 115/(ρ * LH_v0 * h_first_layer) #Evap_flux
         end
