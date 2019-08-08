d707416ecb254bc229e5dd22d5e70222ac514b1a
SIMONE MARRAS <simone@SIMONEs-MacBook-Pro.local>
2019-08-08 18:28:24 -0400

    write vtk 1500 steps

--- test/DGmethods/compressible_Navier_Stokes/dycoms-bcstate.jl
+++ test/DGmethods/compressible_Navier_Stokes/dycoms-bcstate.jl
@@ -858,7 +858,7 @@ function run(mpicomm, dim, Ne, N, timeend, DFloat, dt)
     postprocessarray = MPIStateArray(spacedisc; nstate=npoststates)
 
     step = [0]
-    cbvtk = GenericCallbacks.EveryXSimulationSteps(15000) do (init=false)
+    cbvtk = GenericCallbacks.EveryXSimulationSteps(1500) do (init=false)
         DGBalanceLawDiscretizations.dof_iteration!(postprocessarray, spacedisc, Q) do R, Q, QV, aux
             @inbounds let
                 R[_o_RAD]   = aux[_a_z2inf] + aux[_a_02z]
