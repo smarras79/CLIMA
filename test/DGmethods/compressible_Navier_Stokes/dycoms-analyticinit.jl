ac2c729de168f447700e549a365e49237f487a93
SIMONE MARRAS <simone@SIMONEs-MacBook-Pro.local>
2019-08-15 17:45:07 -0400

    Dycoms: analytic: vtk every 1500 steps. dt = 0.0095

--- test/DGmethods/compressible_Navier_Stokes/dycoms-analyticinit.jl
+++ test/DGmethods/compressible_Navier_Stokes/dycoms-analyticinit.jl
@@ -940,7 +940,7 @@ function run(mpicomm, dim, Ne, N, timeend, DFloat, dt)
     postprocessarray = MPIStateArray(spacedisc; nstate=npoststates)
 
     step = [0]
-    cbvtk = GenericCallbacks.EveryXSimulationSteps(1) do (init=false)
+    cbvtk = GenericCallbacks.EveryXSimulationSteps(1000) do (init=false)
         DGBalanceLawDiscretizations.dof_iteration!(postprocessarray, spacedisc, Q) do R, Q, QV, aux
             @inbounds let
                 R[_o_RAD]   = aux[_a_z2inf] + aux[_a_02z]
